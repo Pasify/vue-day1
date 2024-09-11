14bb09796eafb6b71f3a14cf1927c8b2a97d6c99
paskkal.io <pascalobiora73@gmail.com>
2024-09-11 01:29:49 +0100

    inital commit

---
+++ src/App copy.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Welcome to Your Vue.js App",
+      status: `active`,
+      tasks: ["task1", "task2", "task3"],
+      link: "https://google.com",
+    };
+  },
+  methods: {
+    toggleStatus() {
+      this.status = this.status === "active" ? "pending" : "inactive";
+    },
+  },
+};
+</script>
+<template>
+  <h1>{{ message }}</h1>
+  <p v-if="status === `active`">user is active</p>
+  <p v-else-if="status === `pending`">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <h2>tasks</h2>
+
+  <ul>
+    <li v-for="item in tasks" :key="item">{{ item }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">click for google</a> -->
+  <a :href="link">click for google</a>
+  <button @:click="toggleStatus">change status</button>
+</template>
+
+<style scoped></style>
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("jon doe");
+const status = ref("active");
+const tasks = ref(["task1", "task2", "task3"]);
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+</script>
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === `active`">user is active</p>
+  <p v-else-if="status === `pending`">user is pending</p>
+  <p v-else>user is inactive</p>
+
+  <h2>tasks</h2>
+
+  <ul>
+    <li v-for="item in tasks" :key="item">{{ item }}</li>
+  </ul>
+  <button @:click="toggleStatus">change status</button>
+</template>
